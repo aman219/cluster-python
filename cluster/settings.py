e7741fbc4accce72d704cb4af19f7397588791bc
Aman Patel <65351629+aman219@users.noreply.github.com>
2024-06-08 06:33:30 +0000

    chore: Update .gitignore and add 'file/' directory

--- cluster/settings.py
+++ cluster/settings.py
@@ -36,13 +36,14 @@ INSTALLED_APPS = [
     'audio',
     'video',
     'image',
+    'dummyupload',
+    'dashboard',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dummyupload',
     'corsheaders'
 ]
 
