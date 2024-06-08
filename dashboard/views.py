c23c4e33406a9ebf6dcf7ebd53bed5c86f87cda0
Aman Patel <65351629+aman219@users.noreply.github.com>
2024-06-08 04:15:40 +0000

    chore: Update .gitignore for Django project and remove unused files and directories

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import HttpResponse
 import shutil
 import json
+import os
 # Create your views here.
 
 def home(request):
     return HttpResponse("Home")
 
 def dashboard(request):
-    size = shutil.disk_usage("/home/aman")
+    size = shutil.disk_usage(os.path.expanduser("~"))
     dash = {
         "total": size.total,
         "used": size.used,
